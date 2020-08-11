2aae40814647db6b390def79d6d9a8440868bdea
Gerald Versluis <gerald@verslu.is>
2020-08-11 17:05:54 +0200

    Initial commit

---
+++ MicrochartsSampleXF/MainPage.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microcharts;
+using SkiaSharp;
+using Xamarin.Forms;
+
+namespace MicrochartsSampleXF
+{
+    public partial class MainPage : ContentPage
+    {
+        private readonly ChartEntry[] entries = new[]
+        {
+            new ChartEntry(212)
+            {
+                Label = "UWP",
+                ValueLabel = "112",
+                Color = SKColor.Parse("#2c3e50")
+            },
+            new ChartEntry(248)
+            {
+                Label = "Android",
+                ValueLabel = "648",
+                Color = SKColor.Parse("#77d065")
+            },
+            new ChartEntry(128)
+            {
+                Label = "iOS",
+                ValueLabel = "428",
+                Color = SKColor.Parse("#b455b6")
+            },
+            new ChartEntry(514)
+            {
+                Label = "Forms",
+                ValueLabel = "214",
+                Color = SKColor.Parse("#3498db")
+            }
+        };
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+            chartViewBar.Chart = new BarChart { Entries = entries, ValueLabelOrientation = Orientation.Horizontal, LabelTextSize = 30 };
+            chartViewPie.Chart = new PieChart { Entries = entries, HoleRadius = 0.3f };
+            chartViewLine.Chart = new LineChart { Entries = entries, LineMode = LineMode.Straight };
+        }
+    }
+}
